265d3f02c6064224cd899a67c25a93158e8452f6
Yaroslav Bordovoy <yaroslavbordovoy@gmail.com>
2024-11-27 14:31:46 +0200

    Add status change button, add static and styles

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views import generic
 
@@ -9,6 +11,20 @@ class IndexView(generic.ListView):
     model = Task
     queryset = Task.objects.prefetch_related("tags")
     template_name = "todo/index.html"
+
+    def post(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        task_id = self.request.POST.get("task_id")
+        action = self.request.POST.get("action")
+
+        if task_id:
+            task = Task.objects.get(id=task_id)
+            if action == "complete":
+                task.is_done = True
+            elif action == "undo":
+                task.is_done = False
+            task.save()
+
+        return redirect("todo:index")
 
 
 class TagView(generic.ListView):
