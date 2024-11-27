483674b1f666b22c566906f466559c218b899886
Yaroslav Bordovoy <yaroslavbordovoy@gmail.com>
2024-11-27 13:05:53 +0200

    Add create-update-delete classes for tasks

--- todo/urls.py
+++ todo/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
 
-from todo.views import IndexView, TagView
+from todo.views import (
+    IndexView,
+    TagView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView,
+)
 
 app_name = "todo"
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("tags/", TagView.as_view(), name="tag"),
+    path("task/create/", TaskCreateView.as_view(), name="task-create"),
+    path("task/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("task/<int:pk>/delete", TaskDeleteView.as_view(), name="task-delete"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
 from todo.models import Task, Tag
@@ -14,3 +15,30 @@ class TagView(generic.ListView):
     model = Tag
     queryset = Tag.objects.prefetch_related("tasks")
     template_name = "todo/tag.html"
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = (
+        "content",
+        "deadline",
+        "is_done",
+        "tags",
+    )
+    success_url = reverse_lazy("todo:index")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    fields = (
+        "content",
+        "deadline",
+        "is_done",
+        "tags",
+    )
+    success_url = reverse_lazy("todo:index")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("todo:index")
